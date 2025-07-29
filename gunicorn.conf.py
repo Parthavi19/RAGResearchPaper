73979822c7f0e1a566c619905a535d711bf0065c
Parthavi K <kparthavi1905@gmail.com>
2025-07-29 22:03:17 +0530

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,13 +1,7 @@
-import multiprocessing
+import os
 
-workers = min(multiprocessing.cpu_count() * 2 + 1, 4)
-threads = 2
-worker_class = 'gthread'
-timeout = 120
-max_requests = 1000
-max_requests_jitter = 100
-loglevel = 'info'
-errorlog = '-'
-accesslog = '-'
-preload_app = True
-bind = '0.0.0.0:8080'
+bind = f"0.0.0.0:{os.environ.get('PORT', '8080')}"
+workers = 2
+threads = 4
+timeout = 300  # Increased for model loading
+loglevel = 'debug'
