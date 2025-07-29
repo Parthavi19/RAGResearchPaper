f32176efe8ae1fc2341e3bf02e7bc44dccb3bcde
Parthavi K <kparthavi1905@gmail.com>
2025-07-30 02:47:27 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,2 @@
-import logging
 from app import app
-
-# Configure logging for wsgi.py
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
-logger.info("Initializing WSGI application")
 application = app
-logger.info("WSGI application initialized")
